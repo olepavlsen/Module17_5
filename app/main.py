31e752acb18ea1dcbbebf917a1033456876634b4
olepavlsen <olepavlsen@mail.ru>
2024-12-18 11:57:44 +0300

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from app.routers import user, task
+
+app = FastAPI()
+
+
+@app.get('/')
+async def wellcome():
+    return {"message": "Welcome to Taskmanager"}
+
+
+app.include_router(user.router)
+app.include_router(task.router)
+
+# uvicorn app.main:app
